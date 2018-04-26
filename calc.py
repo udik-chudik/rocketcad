c177b41cbfbac9a441f7f3cb0a4855130aae330e
Andrey Yudaev <ua@veryfood.ru>
2018-04-26 20:40:46 +0300

    Solution saving, viewer separated

--- calc.py
+++ calc.py
@@ -1,10 +1,6 @@
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-
 from ElectronRocket import Electron
 from Physics import *
-import copy
-
+import numpy as np
 from scipy.integrate import ode
 
 rock = Electron()
@@ -26,59 +22,10 @@ sol = []
 times = []
 while r.successful() and r.t < tMax:
 	s = r.integrate(r.t+dt)
-	sol.append([r.t, s[0],s[1],s[2],s[3],s[4],s[5],s[6],s[7],s[8],s[9],s[10],s[11]])
-	#print(r.t+dt, r.integrate(r.t+dt))
+	sol.append([r.t, s[0],s[1],s[2],s[3],s[4],s[5],s[6],s[7],s[8],s[9],s[10],s[11], rock.getMass()])
 
 	rock.tick(dt)
 
 sol = np.array(sol)
 
 np.save('solution', sol)
-
-ax = plt.subplot(111, aspect=1)
-
-
-
-ax.plot(sol[:,1], sol[:,2])
-
-earth = mpatches.Circle((0, 0), 6400000, fc="g")
-atmosphere = mpatches.Circle((0, 0), 6500000, facecolor='none', edgecolor='r', linestyle='--')
-ax.add_patch(earth)
-ax.add_patch(atmosphere)
-
-
-plt.show()
-
-"""
-
-
-tMax = 1
-dt = 0.1
-
-t = 0
-
-times = []
-
-f = np.array([0,6400000,0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-data = np.array([f])
-
-
-while t < tMax:
-
-	rock.tick(dt)
-
-	delta = ph.dynamics6d(f)
-
-
-	f = f + copy.copy(delta*dt)
-
-	t = t + dt
-
-	times.append(t)
-	data = np.append(data, f)
-
-print(data)
-"""
-#plt.plot(times, data[0,:])
-#plt.show()
---
+++ view.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import sys
+
+sol = np.load('solution.npy')
+
+ax = plt.subplot(111, aspect=1)
+
+if (len(sys.argv) == 1):
+
+	ax.plot(sol[:,1], sol[:,2])
+
+	earth = mpatches.Circle((0, 0), 6400000, fc="g")
+	atmosphere = mpatches.Circle((0, 0), 6500000, facecolor='none', edgecolor='r', linestyle='--')
+	ax.add_patch(earth)
+	ax.add_patch(atmosphere)
+	plt.show()
+
+
+elif (sys.argv[1] == 'V'):
+
+	ax.plot(sol[:,0], np.sqrt(sol[:,4]**2 + sol[:,5]**2 + sol[:,6]**2))
+	plt.show()
+
+elif (sys.argv[1] == 'M'):
+
+	ax.plot(sol[:,0], sol[:,13])
+	plt.show()
+
+elif (sys.argv[1] == 'H'):
+
+	ax.plot(sol[:,0], (np.sqrt(sol[:,1]**2 + sol[:,2]**2 + sol[:,3]**2) - 6400000)/1000)
+	plt.show()
